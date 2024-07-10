1173c37a08e7c1678f3489c779f2d908c20282d4
atichat <atichatfantasy@gmail.com>
2024-07-10 23:24:13 +0700

    rename folder modules to feature, Added shared and services

--- frontend/src/app/features/home/home.component.ts
+++ frontend/src/app/features/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ButtonComponent } from '../../shared/button/button.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, SharedModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

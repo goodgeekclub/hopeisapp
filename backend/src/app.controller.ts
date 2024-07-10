e4ce5e0afb81ad6bdc2f433644c6033baf770e3a
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-07-11 00:33:18 +0700

    feat: connect mongodb atlas

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -4,8 +4,12 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get('test')
+  getTest() {
+    return this.appService.getCollections();
+  }
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class AppService {
+  constructor(@InjectConnection() private connection: Connection) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getCollections() {
+    return this.connection.listCollections();
   }
 }

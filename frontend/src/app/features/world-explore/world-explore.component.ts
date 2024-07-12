f348b1e859811bfb67ead60f2b2dad49dd5bb625
auto2557 <auto16348@gmail.com>
2024-07-12 17:06:37 +0700

    Create World_Explore_Component

---
+++ frontend/src/app/features/world-explore/world-explore.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-world-explore',
+  standalone: true,
+  imports: [],
+  templateUrl: './world-explore.component.html',
+  styleUrl: './world-explore.component.css'
+})
+export class WorldExploreComponent {
+
+}

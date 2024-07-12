224012b6d8fe5bef2bfea4bdc15a92b9c3f98d0d
viruskizz <kizzaraiva@gmail.com>
2024-07-13 00:48:49 +0700

    doc: add swagger doc

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { MongooseInterceptor } from './interceptors/mongoose.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new MongooseInterceptor());
+
+  // Setup Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Hopeis Backend API')
+    .setDescription('This is backend for data of Hopeis application')
+    .setVersion('0.1')
+    .addTag('Quiz')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

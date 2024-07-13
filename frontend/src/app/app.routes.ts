a6eb565c3e4db141a681b18a3cef61fb9288e58a
atichat <atichatfantasy@gmail.com>
2024-07-13 16:53:22 +0700

    pixi.js init

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { WorldExploreComponent } from './features/world-explore/world-explore.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
   },
+  {
+    path: 'world-explore',
+    component: WorldExploreComponent
+  }
 ];

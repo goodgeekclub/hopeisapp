9218adff87ebe1f63f189fe8a874dd0450900a1e
auto2557 <auto16348@gmail.com>
2024-07-13 21:52:08 +0700

    spawn_stars

--- frontend/src/app/features/world-explore/world-explore.component.ts
+++ frontend/src/app/features/world-explore/world-explore.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as PIXI from 'pixi.js';
 
 @Component({
   selector: 'app-world-explore',
   standalone: true,
   imports: [],
   templateUrl: './world-explore.component.html',
-  styleUrl: './world-explore.component.css'
+  styleUrls: ['./world-explore.component.css'],
 })
-export class WorldExploreComponent {
+export class WorldExploreComponent implements OnInit {
+  stars: PIXI.Graphics[] = [];
+  async ngOnInit() {
+    if (typeof window !== 'undefined') {
+      
+      
+      const app = new PIXI.Application();
 
-}
+   
+      await app.init({ background: '#000000', resizeTo: window });
+
+      
+      document.body.appendChild(app.canvas);
+
+      for (let i = 0; i < 1000; i++) {
+        const star = this.createStar(app);
+        this.stars.push(star);
+      }
+    }
+  }
+
+  createStar(app: PIXI.Application): PIXI.Graphics {
+    
+    const star = new PIXI.Graphics();
+
+    
+    const size = Math.random() * 2 + 1;
+
+   
+    const color = Math.random() * 0xffffff;
+
+  
+    const x = Math.random() * app.renderer.width;
+    const y = Math.random() * app.renderer.height;
+
+    
+    star.beginFill(color);
+    star.drawStar(0, 0, 5, size);
+    star.endFill();
+
+    
+    star.x = x;
+    star.y = y;
+
+   
+    app.stage.addChild(star);
+
+    return star;
+      
+    }
+  }

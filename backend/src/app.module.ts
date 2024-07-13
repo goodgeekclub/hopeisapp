1543c6701b40f95dec3e06dd9858a43f249ed988
Mabel <mobile44.mm@gmail.com>
2024-07-14 00:21:06 +0700

    fix: see pr comments

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,15 +5,12 @@ import { ConfigModule } from '@nestjs/config';
 import { mongooseConnection } from './configs/mongoose.config';
 import { QuizesModule } from './features/quizes/quizes.module';
 import { ProfilesModule } from './features/profiles/profiles.module';
-import { AbcModule } from './features/abc/abc.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
     mongooseConnection.root,
     QuizesModule,
     ProfilesModule,
-    AbcModule,
   ],
   controllers: [AppController],
   providers: [AppService],

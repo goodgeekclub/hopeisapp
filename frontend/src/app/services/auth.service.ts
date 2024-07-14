162dbbfacf787e1db22caf18c2ca8b0eeb02bd7e
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-07-14 23:49:14 +0700

    feat: setup test script assume oidc

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -22,8 +22,24 @@ export class AuthService {
       this.auth,
       this.googleAuthProvider
     )
+<<<<<<< HEAD
       .then(async (result) => {
         return this.auth.currentUser;
+=======
+      .then((result) => {
+        console.log('Result', result)
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+
+        const user: User = {
+          displayName: result?.user?.displayName || '',
+          email: result?.user?.email || '',
+          phoneNumber: result?.user?.phoneNumber || undefined,
+          photoURL: result?.user?.photoURL || undefined,
+          token: credential?.accessToken || '',
+        };
+
+        return user;
+>>>>>>> 72fcec1 (feat: setup test script assume oidc)
       })
       .catch((error) => {
         console.error('AuthService.register', error.message);
@@ -32,3 +48,16 @@ export class AuthService {
     return registeredUser;
   }
 }
+
+// {
+//   "Effect": "Allow",
+//   "Principal": {
+//     "Federated": "arn:aws:iam::907877978309:oidc-provider/securetoken.google.com/520520413022"
+//   },
+//   "Action": "sts:AssumeRoleWithWebIdentity",
+//   "Condition": {
+//     "StringEquals": {
+//       "securetoken.google.com/520520413022:aud": "520520413022"
+//     }
+//   }
+// },

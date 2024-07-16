37f34077950b2ad15b2098594dc340adddfdfc69
atichat <atichatfantasy@gmail.com>
2024-07-16 12:17:32 +0700

    feat: add modal popup and prevent saving null or empty name input

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,29 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { NameInputComponent } from './features/name-input/name-input.component';
+import { QuizStartComponent } from './features/quiz-start/quiz-start.component';
+import { QuizComponent } from './features/quiz/quiz.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    redirectTo: 'enter-name',
+    pathMatch: 'full',
   },
   {
-    path: 'test-name-input',
+    path: 'enter-name',
     component: NameInputComponent,
-  }
+  },
+  {
+    path: 'start-quiz',
+    component: QuizStartComponent,
+  },
+  {
+    path: 'quiz',
+    component: QuizComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'enter-name',
+  },
 ];
---
+++ frontend/src/app/features/quiz-start/quiz-start.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-quiz-start',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-start.component.html',
+  styleUrl: './quiz-start.component.css',
+})
+export class QuizStartComponent {
+  showModal: boolean = false;
+
+  constructor(private router: Router) {}
+
+  startQuiz() {
+    this.showModal = true;
+  }
+
+  confirmStart() {
+    this.showModal = false;
+    this.router.navigate(['/quiz']);
+  }
+
+  cancelStart() {
+    this.showModal = false;
+  }
+}

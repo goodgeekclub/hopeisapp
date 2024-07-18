02e26cf5d47ffd49a2082ef3668e156c87177431
tliangso <earth78203@gmail.com>
2024-07-19 01:53:04 +0700

    fix uncaught error on invalid token

--- backend/src/auth/strategies/firebase.strategy.ts
+++ backend/src/auth/strategies/firebase.strategy.ts
@@ -2,7 +2,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Strategy, ExtractJwt } from 'passport-firebase-jwt';
 import { auth } from 'firebase-admin';
-import { catchError, from, map } from 'rxjs';
+import { catchError, from, map, of } from 'rxjs';
 
 @Injectable()
 export class FirebaseAuthStrategy extends PassportStrategy(
@@ -16,13 +16,12 @@ export class FirebaseAuthStrategy extends PassportStrategy(
   }
 
   validate(token: string) {
-    return from(auth().verifyIdToken(token, true)).pipe(
-    map((decodedToken) => {
-        return !!decodedToken;
-      }),
-      catchError((error) => {
-        throw new UnauthorizedException(error);
-      }),
-    );
+    // verifyIdToken throws an error which cannot be caught by the catchError operator in the Observable so we use .then and .catch
+    return auth()
+      .verifyIdToken(token, true)
+      .then((res) => !!res)
+      .catch((e) => {
+        throw new UnauthorizedException('Invalid token');
+      });
   }
 }

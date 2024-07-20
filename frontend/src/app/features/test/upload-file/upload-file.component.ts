7371b078c241a51fd1945d1fd4cf5cc67a439752
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-07-20 13:56:36 +0700

    feat: preview image

--- frontend/src/app/features/test/upload-file/upload-file.component.ts
+++ frontend/src/app/features/test/upload-file/upload-file.component.ts
@@ -8,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './upload-file.component.css'
 })
 export class UploadFileComponent {
+  status: "initial" | "uploading" | "success" | "fail" = "initial";
+  file?: File | null;
+  limit = {
+    types: ['image/jpeg', 'images/png'],
+    size: 1 * 1024 * 1024,
+  }
+  imageUrl: string | ArrayBuffer | null = null;
 
+  changeUpload(event: Event) {
+    if (event.target) {
+      const input = event.target as HTMLInputElement;
+      if (input.files && input.files[0]) {
+        const file: File = input.files[0];
+        console.log(file);
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = (e) => {
+          this.imageUrl = e.target!.result;
+        }
+      }
+    }
+  }
+
+  onUpload() {
+
+  }
 }

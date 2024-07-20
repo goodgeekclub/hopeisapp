15a3f2b033a74d6161da9f36aa94d410533632f7
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-07-21 00:24:15 +0700

    Implement result page structure

--- frontend/src/app/features/test/result-test/result-test.component.ts
+++ frontend/src/app/features/test/result-test/result-test.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-result-test',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, CommonModule, SvgIconComponent ],
   templateUrl: './result-test.component.html',
-  styleUrl: './result-test.component.css'
+  styleUrl: './result-test.component.css',
 })
-export class ResultTestComponent {
+export class ResultTestComponent implements OnInit {
+  public hasResult = false;
+  public isClickedToShowResult = false;
+  constructor() {}
+  ngOnInit(): void {
+    this.getResult();
+  }
 
+  public showResult(): void {
+    if (!this.hasResult) return;
+    this.isClickedToShowResult = true;
+  }
+
+  private getResult(): void {
+    setTimeout(() => {
+      this.hasResult = true;
+    }, 3000);
+  }
 }

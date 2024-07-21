d95a4b1ce5358bba8164531940baa4630a4d504b
Mabel <mobile44.mm@gmail.com>
2024-07-21 19:46:32 +0700

    [Backend] Update profile schema

--- backend/src/features/profiles/dto/create-profile.dto.ts
+++ backend/src/features/profiles/dto/create-profile.dto.ts
@@ -1,18 +1,88 @@
-import { ApiExtraModels, ApiProperty, PartialType } from '@nestjs/swagger';
+import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
 import { Profile } from 'src/schemas/profile.schema';
-import { IsString } from 'class-validator';
+import {
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
 
-@ApiExtraModels(Profile)
-export class CreateProfileDto extends PartialType(Profile) {
+class MetaDataDTO {
+  @IsNumber()
+  @ApiProperty({ example: 12 })
+  totalCoin: number;
+
+  @IsNumber()
+  @ApiProperty({ example: 100 })
+  missionSuccess: number;
+}
+
+class CharacterDTO {
   @IsString()
-  @ApiProperty({ example: 'mobi' })
+  @ApiProperty({ example: 'id' })
+  id: string;
+
+  @IsString()
+  @ApiProperty({ example: 'name' })
+  name: string;
+
+  @IsString()
+  @ApiProperty({ example: 'photo_url' })
+  photoUrl: string;
+
+  @IsString()
+  @ApiProperty({ example: 'detail' })
+  detail: string;
+}
+@ApiExtraModels(Profile)
+export class CreateProfileDto {
+  @IsString()
+  @ApiProperty({ example: 'display_name' })
+  displayName: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ example: 'first_name' })
   firstName: string;
 
   @IsString()
-  @ApiProperty({ example: 'le' })
+  @IsOptional()
+  @ApiProperty({ example: 'last_name' })
   lastName: string;
 
   @IsString()
-  @ApiProperty({ example: 'mobile@gmail.com' })
+  @IsOptional()
+  @ApiProperty({ example: 'example@mail.com' })
   email: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ example: 'female' })
+  gender: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ example: new Date() })
+  birthday: Date;
+
+  @ValidateNested()
+  @Type(() => MetaDataDTO)
+  @ApiProperty({ type: MetaDataDTO })
+  metadata: MetaDataDTO;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ example: 'photo_url' })
+  photoUrl: string;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ example: 'firebase_id' })
+  firebaseId: string;
+
+  @ValidateNested()
+  @Type(() => CharacterDTO)
+  @ApiProperty({ type: CharacterDTO })
+  character: CharacterDTO;
 }
--- backend/src/schemas/profile.schema.ts
+++ backend/src/schemas/profile.schema.ts
@@ -2,21 +2,86 @@ import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 import { HydratedDocument } from 'mongoose';
 
+@Schema()
+class MetaData {
+  @Prop({ required: true })
+  @ApiProperty({ example: 12 })
+  totalCoin: number;
+
+  @Prop({ required: true })
+  @ApiProperty({ example: 100 })
+  missionSuccess: number;
+}
+
+@Schema()
+class Character {
+  @Prop({ required: true })
+  @ApiProperty({ example: 'id' })
+  id: string;
+
+  @Prop({ required: true })
+  @ApiProperty({ example: 'name' })
+  name: string;
+
+  @Prop({ required: true })
+  @ApiProperty({ example: 'photo_url' })
+  photoUrl: string;
+
+  @Prop({ required: true })
+  @ApiProperty({ example: 'detail' })
+  detail: string;
+}
 @Schema({
   timestamps: true,
 })
 export class Profile {
-  @Prop({ unique: true, required: true })
-  @ApiProperty({ example: 'example' })
+  @Prop({ required: true })
+  @ApiProperty({ example: 'display_name' })
+  displayName: string;
+
+  @Prop()
+  @ApiProperty({ example: 'first_name' })
   firstName: string;
 
-  @Prop({ required: true })
-  @ApiProperty({ example: 'example' })
+  @Prop()
+  @ApiProperty({ example: 'last_name' })
   lastName: string;
 
-  @Prop({ unique: true, required: true })
+  @Prop()
   @ApiProperty({ example: 'example@mail.com' })
   email: string;
+
+  @Prop()
+  @ApiProperty({ example: 'female' })
+  gender: string;
+
+  @Prop()
+  @ApiProperty({ example: new Date() })
+  birthday: Date;
+
+  @Prop()
+  @ApiProperty({ example: new Date() })
+  createdAt: Date;
+
+  @Prop()
+  @ApiProperty({ example: new Date() })
+  updatedAt: Date;
+
+  @Prop({ required: true })
+  // @ApiProperty({ example: 'metadata' })
+  metadata: MetaData;
+
+  @Prop()
+  @ApiProperty({ example: 'photo_url' })
+  photoUrl: string;
+
+  @Prop()
+  @ApiProperty({ example: 'firebase_id' })
+  firebaseId: string;
+
+  @Prop({ required: true })
+  // @ApiProperty({ example: 'character' })
+  character: Character;
 }
 
 export type ProfileDocument = HydratedDocument<Profile>;

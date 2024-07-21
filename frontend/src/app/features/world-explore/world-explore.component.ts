5b2096ffeef6161138b3a1a415be7ac34caf4929
auto2557 <auto16348@gmail.com>
2024-07-21 23:22:41 +0700

    galaxy-background

--- frontend/src/app/features/world-explore/world-explore.component.ts
+++ frontend/src/app/features/world-explore/world-explore.component.ts
@@ -18,28 +18,42 @@ export class WorldExploreComponent implements OnInit {
   stars: PIXI.Graphics[] = [];
   users: User[] = [];
   batchSize: number = 1; // Number of stars to create per batch
-  displayLimit: number = 1000; //limit user display
+  displayLimit: number = 250; //limit user display
   
 
 
   async ngOnInit() {
 
+    const app = new PIXI.Application();
     const numberOfUsers = 100000; // test Number of user
     for (let i = 1; i <= numberOfUsers; i++) {
       this.users.push({ id: i, name: `User ${i}` });
     }
 
-    if (typeof window !== 'undefined') {
-      
-      
-      const app = new PIXI.Application();
-
-   
-      await app.init({ background: '#000000', resizeTo: window });
-
-      
+    app.init({ resizeTo: window }).then(() => {
+      // Append the application canvas to the document body
       document.body.appendChild(app.canvas);
-      
+  
+      // Add the asset to load
+      PIXI.Assets.add({ alias: 'background', src: 'https://i0.wp.com/www.sciencenews.org/wp-content/uploads/2023/03/032723_bdh_habitable-exomoons_feat.jpg?fit=1030%2C580&ssl=1' });
+  
+      // Allow the asset to load in the background
+      PIXI.Assets.backgroundLoad(['background']);
+  
+      // Load the background image
+      PIXI.Assets.load('background').then((backgroundTexture) => {
+          // Create a new Sprite for the background
+          const background = new PIXI.Sprite(backgroundTexture);
+  
+          // Set the background to cover the whole screen
+          background.width = app.screen.width;
+          background.height = app.screen.height;
+  
+          // Add the background to the stage
+          app.stage.addChild(background);
+      });
+
+
       let batchIndex = 0;
       const createNextBatch = () => {
         if (batchIndex < this.users.length && this.stars.length < this.displayLimit) {
@@ -57,12 +71,13 @@ export class WorldExploreComponent implements OnInit {
       };
       createNextBatch();
     }
-  }
+  )}
+
 
   createStar(app: PIXI.Application, user: User): PIXI.Graphics {
     const star = new PIXI.Graphics();
     const size = Math.random() * 5 + 3;
-    const color = Math.random() * 0xffffff;
+    const color = Math.random() * 0x000000;
   
     let x: number;
     let y: number;
@@ -78,7 +93,7 @@ export class WorldExploreComponent implements OnInit {
     } while (overlap);
   
     star.beginFill(color);
-    star.drawStar(0, 0, 5, size);
+    star.drawStar(0, 0, 4, size);
     star.endFill();
   
     star.x = x;

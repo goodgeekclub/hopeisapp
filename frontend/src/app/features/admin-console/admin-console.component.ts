14e9e1c9ef9834dfb5d49d5613f4d2cabca08d2b
MyNNeiei <kullananzaza@gmail.com>
2024-07-22 18:02:07 +0700

    UIfinishdonthaveDB

--- frontend/src/app/features/admin-console/admin-console.component.ts
+++ frontend/src/app/features/admin-console/admin-console.component.ts
@@ -1,7 +1,6 @@
 import { Component ,OnInit } from '@angular/core';
 import { User } from '../../models/user.model';
 import { AdminUserService } from '../../services/admin_user.service';
-
 @Component({
   selector: 'app-admin-console',
   standalone: true,
@@ -10,51 +9,176 @@ import { AdminUserService } from '../../services/admin_user.service';
   styleUrl: './admin-console.component.css',
 })
 
-export class AdminConsole {
+export class AdminConsole implements OnInit{
+  
   users: Array<User> =[
     {
       displayName: 'Kullanan',
       email: 'kullananzaza@gmail.com',
       token: '34534535353',
-      phoneNumber: '5398309-5830985'
+      phoneNumber: '0649932001'
     },
     {
       displayName: 'Art',
       email: 'Art@gmail.com',
       token: '34534535353',
-      phoneNumber: '5398309-5830985'
+      phoneNumber: '0649932001'
     },
     {
       displayName: 'Kullanan',
-      email: 'kullananzaza@gmail.com',
+      email: 'NNnam1.privatemail@gmail.com',
       token: '34534535353',
-      phoneNumber: '5398309-5830985'
+      phoneNumber: '0649932001'
     },
     {
-      displayName: 'Art',
-      email: 'Art@gmail.com',
+      displayName: 'Kullanan',
+      email: 'NNnam1.privatemail@gmail.com',
       token: '34534535353',
-      phoneNumber: '5398309-5830985'
+      phoneNumber: '0649932001'
     },
     {
-      displayName: 'Art',
-      email: 'Art@gmail.com',
+      displayName: 'Nay',
+      email: 'Araiva@gmail.com',
+      token: 'fssffsfsf',
+      phoneNumber: '08573829902'
+    },
+    {
+      displayName: 'Kullanan',
+      email: 'NNnam1.privatemail@gmail.com',
       token: '34534535353',
-      phoneNumber: '5398309-5830985'
+      phoneNumber: '0649932001'
+    },
+    {
+      displayName: 'Nay',
+      email: 'Araiva@gmail.com',
+      token: 'fssffsfsf',
+      phoneNumber: '08573829902'
+    },
+    {
+      displayName: 'Kullanan',
+      email: 'NNnam1.privatemail@gmail.com',
+      token: '34534535353',
+      phoneNumber: '0649932001'
+    },
+    {
+      displayName: 'Nay',
+      email: 'Araiva@gmail.com',
+      token: 'fssffsfsf',
+      phoneNumber: '08573829902'
+    },{
+      displayName: 'Kullanan',
+      email: 'NNnam1.privatemail@gmail.com',
+      token: '34534535353',
+      phoneNumber: '0649932001'
+    },
+    {
+      displayName: 'Nay',
+      email: 'Araiva@gmail.com',
+      token: 'fssffsfsf',
+      phoneNumber: '08573829902'
+    },{
+      displayName: 'Kullanan',
+      email: 'NNnam1.privatemail@gmail.com',
+      token: '34534535353',
+      phoneNumber: '0649932001'
+    },
+    {
+      displayName: 'Nay',
+      email: 'Araiva@gmail.com',
+      token: 'fssffsfsf',
+      phoneNumber: '08573829902'
+    },{
+      displayName: 'Kullanan',
+      email: 'NNnam1.privatemail@gmail.com',
+      token: '34534535353',
+      phoneNumber: '0649932001'
+    },
+    {
+      displayName: 'Nay',
+      email: 'Araiva@gmail.com',
+      token: 'fssffsfsf',
+      phoneNumber: '08573829902'
+    },{
+      displayName: 'Kullanan',
+      email: 'NNnam1.privatemail@gmail.com',
+      token: '34534535353',
+      phoneNumber: '0649932001'
+    },
+    {
+      displayName: 'Nay',
+      email: 'Araiva@gmail.com',
+      token: 'fssffsfsf',
+      phoneNumber: '08573829902'
+    },{
+      displayName: 'Kullanan',
+      email: 'NNnam1.privatemail@gmail.com',
+      token: '34534535353',
+      phoneNumber: '0649932001'
+    },
+    {
+      displayName: 'Nay',
+      email: 'Araiva@gmail.com',
+      token: 'fssffsfsf',
+      phoneNumber: '08573829902'
+    },
+    {
+      displayName: 'Nay',
+      email: 'Araiva@gmail.com',
+      token: 'fssffsfsf',
+      phoneNumber: '08573829902'
     },
   ]
-  // user: Array<User> = []
-  // constructor(private adminUserService : AdminUserService){}
-  
-  // ngOnInit(): void {
-  //   this.loadData();
-  // }
 
-  // loadData(): void {
-  //   this.adminUserService.getData()
-  //     .subscribe(data => {
-  //       this.user = data;
-  //     });
-  // }
+  currentPage : number = 1;
+  pageSize : number = 10;
+
+  filterUsers: Array<User> = this.users;
+  pageSizes: Array<number> = [ 5, 10, 20 ]
+  
+  ngOnInit(): void {
+    this.visibleData();
+    // this.pageNumber();
+
+  }
+  visibleData(){
+    let startIndex = (this.currentPage -1)* this.pageSize ;
+    let endIndex = startIndex + this.pageSize;
+    return this.filterUsers.slice(startIndex, endIndex);
+  }
+
+  nextPage(){
+    this.currentPage++;
+    this.visibleData();
+  }
+
+  previousPage(){
+    this.currentPage--;
+    if(this.currentPage == 0){
+      this.currentPage = 1;
+    }
+    this.visibleData();
+  }
+
+
+  changePage(pageNumber:number){
+    this.currentPage = pageNumber;
+    this.visibleData();
+  }
+  filterData(searchTerm:string){
+    if (searchTerm) {
+      this.filterUsers = this.users.filter(user =>
+        Object.values(user).some(val =>
+          val.toString().toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      );
+    } else {
+      this.filterUsers = [...this.users];
+    }
+    this.visibleData();
+  }
+  changePageSize(pageSize:any){
+    this.pageSize = pageSize;
+    this.visibleData();
+  }
 
 }

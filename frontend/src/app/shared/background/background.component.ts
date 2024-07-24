41581f9a3277be26a4cd8b8863a8ab43a5dd384b
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-07-25 00:40:56 +0700

    Implement UI following figma

---
+++ frontend/src/app/shared/background/background.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrl: './background.component.css'
+})
+export class BackgroundComponent {
+  @Input() public src!: string;
+}

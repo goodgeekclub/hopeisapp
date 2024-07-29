0d0d419ce3f7c5ce57f0c1b2d56b32a9e8ac5420
JM <pj.jirasin@gmail.com>
2024-07-29 13:04:09 +0700

    feat: add datadog role

--- iac/web/iam.tf
+++ iac/web/iam.tf
@@ -1,3 +1,6 @@
+########################
+# Federated
+########################
 data "aws_iam_policy_document" "role_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -58,6 +61,92 @@ resource "aws_iam_role" "federate_role" {
             "arn:aws:s3:::${local.buckets.asset.name}/*"
           ]
         }
+      ]
+    })
+  }
+}
+
+########################
+# DataDog
+########################
+data "aws_iam_policy_document" "datadog_trust_policy" {
+  count = var.environment == "dev" ? 1 : 0
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["464622532012"] # DataDog Account ID
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+      values = [
+        "0ab25ab94c48473a87fb55079020f8d1" # Datadog External ID
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "datadog_role" {
+  count              = var.environment == "dev" ? 1 : 0
+  name               = "${local.project_name}-datadog-role"
+  description        = "Allow DataDog access to hopeisapp production environment only"
+  assume_role_policy = data.aws_iam_policy_document.datadog_trust_policy[0].json
+
+  inline_policy {
+    name = "${local.project_name}-datadog-integration-policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = [
+            "apigateway:GET",
+            "cloudfront:GetDistributionConfig",
+            "cloudfront:ListDistributions",
+            "cloudtrail:DescribeTrails",
+            "cloudtrail:GetTrailStatus",
+            "cloudtrail:LookupEvents",
+            "cloudwatch:Describe*",
+            "cloudwatch:Get*",
+            "cloudwatch:List*",
+            "events:CreateEventBus",
+            "health:DescribeEvents",
+            "health:DescribeEventDetails",
+            "health:DescribeAffectedEntities",
+            "lambda:GetPolicy",
+            "lambda:List*",
+            "logs:DeleteSubscriptionFilter",
+            "logs:DescribeLogGroups",
+            "logs:DescribeLogStreams",
+            "logs:DescribeSubscriptionFilters",
+            "logs:FilterLogEvents",
+            "logs:PutSubscriptionFilter",
+            "logs:TestMetricFilter",
+            "route53:List*",
+            "s3:GetBucketLogging",
+            "s3:GetBucketLocation",
+            "s3:GetBucketNotification",
+            "s3:GetBucketTagging",
+            "s3:ListAllMyBuckets",
+            "s3:PutBucketNotification",
+            "support:DescribeTrustedAdvisor*",
+            "support:RefreshTrustedAdvisorCheck",
+            "tag:GetResources",
+            "tag:GetTagKeys",
+            "tag:GetTagValues",
+            "xray:BatchGetTraces",
+            "xray:GetTraceSummaries"
+          ]
+          Effect   = "Allow"
+          Resource = "*",
+          "Condition": {
+            "StringEquals": {
+              "aws:RequestedRegion": "ap-southeast-1"
+            }
+          }
+        },
       ]
     })
   }

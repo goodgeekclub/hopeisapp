b141558aa54bcb7776d2239a6970a33b9571a476
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-08-01 22:09:21 +0700

    Edit layout

--- frontend/src/app/features/mission/mission/mission.component.ts
+++ frontend/src/app/features/mission/mission/mission.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-mission',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, SvgIconComponent],
   templateUrl: './mission.component.html',
   styleUrl: './mission.component.css'
 })

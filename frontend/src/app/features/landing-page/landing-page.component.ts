188d8506cbb0523f8655b1f9cefb277ef5ddbafb
Salamander <mokmaard646@gmail.com>
2024-08-04 23:00:42 +0700

    add share api in component.ts

--- frontend/src/app/features/landing-page/landing-page.component.ts
+++ frontend/src/app/features/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,19 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
+
 export class LandingPageComponent {
   total_users : string = "50k+";
   total_hope_risers : string = "1000";
   //please edit below links;
-  share_link : string = "https://www.google.com";
   ig_link : string = "http://ig";
   website_link : string = "http/website";
   mission_link : string = "";
   let_do_the_test_link : string = "";
-  
+  //share
+  title : string = "มาหาเเสงของตัวเองกันเถอะ";
+  url : string = "http....";
+
   ngOnInit () {
     this.total_users = this.numberToKString(50000);
     this.total_hope_risers = this.numberFormat(1000);
+  }
+
+  onClick() {
+    if ( navigator.share ) {
+      navigator.share(
+        {
+          title : this.title,
+          url : this.url
+        }
+      ).then(() => {
+        console.log("hello world");
+      })
+    } else {
+      navigator.clipboard.writeText(this.url);
+      alert("copied url: " + this.url );
+    }
   }
 
   private numberToKString(num: number): string {

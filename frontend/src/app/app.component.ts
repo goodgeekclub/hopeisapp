2d04afea66253114a33e6a96ddb7f3e411d693e8
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-08-05 01:54:50 +0700

    ci: add fmc service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Analytics, logEvent, setCurrentScreen } from '@angular/fire/analytics';
+import { FcmService } from './services/fcm.service';
+import { Analytics, logEvent } from '@angular/fire/analytics';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -6,10 +7,6 @@ import { SvgIconComponent } from 'angular-svg-icon';
 import { AuthService } from './services';
 import { environment } from '../environments/environment';
 import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
-import { getToken, Messaging, onMessage } from '@angular/fire/messaging';
-import { Observable, tap } from 'rxjs';
-import { onBackgroundMessage } from "firebase/messaging/sw";
-
 
 @Component({
   selector: 'app-root',
@@ -27,8 +24,7 @@ import { onBackgroundMessage } from "firebase/messaging/sw";
 export class AppComponent implements OnInit {
   title = 'frontend';
   private analytics = inject(Analytics);
-  private messaging = inject(Messaging);
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private FcmService: FcmService) {
   }
 
   ngOnInit(): void {
@@ -38,43 +34,6 @@ export class AppComponent implements OnInit {
         console.log(user);
         console.log('accessToken', await user.getIdToken());
       }
-    });
-    console.log(Notification.permission)
-    Notification.requestPermission().then(
-      (notificationPermissions: NotificationPermission) => {
-        if (notificationPermissions === "granted") {
-          console.log("Granted");
-        }
-        if (notificationPermissions === "denied") {
-          console.log("Denied");
-        }
-      }).catch(e => {
-        console.log(e);
-      });
-    this.requestPermission();
-    this.listen();
-  }
-
-  requestPermission() {
-    getToken(this.messaging, {vapidKey: environment.vapidKey})
-      .then((currentToken) => {
-        if (currentToken) {
-          console.log('FCM Token:', currentToken);
-        } else {
-          console.log('No registration token available. Request permission to generate one.');
-        }
-      }).catch((err) => {
-        console.log('Error:', err);
-      });
-  }
-  listen() {
-    onMessage(this.messaging, {
-      next: (payload) => {
-        console.log(payload);
-        alert(JSON.stringify(payload.notification))
-      },
-      error: (error) => console.log('Message error', error),
-      complete: () => console.log('Done listening to messages'),
     });
   }
 }
--- frontend/src/app/services/fcm.service.ts
+++ frontend/src/app/services/fcm.service.ts
@@ -1,33 +1,48 @@
 import { Injectable } from "@angular/core";
 import { Messaging, getToken, onMessage, deleteToken } from "@angular/fire/messaging";
-import { Observable, tap } from "rxjs";
+import { environment } from "../../environments/environment.development";
 
 @Injectable({
   providedIn: "root",
 })
 export class FcmService {
-  constructor(private msg: Messaging){
+  constructor(private messaging: Messaging){
+    console.log(Notification.permission)
     Notification.requestPermission().then(
-    (notificationPermissions: NotificationPermission) => {
-      if (notificationPermissions === "granted") {
-        console.log("Granted");
-      }
-      if (notificationPermissions === "denied") {
-        console.log("Denied");
-      }
-    });
-    navigator.serviceWorker
-      .register("/assets/firebase-messaging-sw.js", {
-        type: "module",
-      })
-      .then((serviceWorkerRegistration) => {
-        getToken(this.msg, {
-          vapidKey: `an optional key generated on Firebase for your fcm tokens`,
-          serviceWorkerRegistration: serviceWorkerRegistration,
-        }).then((x) => {
-          console.log('my fcm token', x);
-          // This is a good place to then store it on your database for each user
-        });
+      (notificationPermissions: NotificationPermission) => {
+        if (notificationPermissions === "granted") {
+          console.log("Granted");
+        }
+        if (notificationPermissions === "denied") {
+          console.log("Denied");
+        }
+      }).catch(e => {
+        console.log(e);
+      });
+    this.requestPermission();
+    this.listen();
+  }
+
+  requestPermission() {
+    getToken(this.messaging, {vapidKey: environment.vapidKey})
+      .then((currentToken) => {
+        if (currentToken) {
+          console.log('FCM Token:', currentToken);
+        } else {
+          console.log('No registration token available. Request permission to generate one.');
+        }
+      }).catch((err) => {
+        console.log('Error:', err);
+      });
+  }
+  listen() {
+    onMessage(this.messaging, {
+      next: (payload) => {
+        console.log(payload);
+        alert(JSON.stringify(payload.notification))
+      },
+      error: (error) => console.log('Message error', error),
+      complete: () => console.log('Done listening to messages'),
     });
   }
 }

e92e84f05b5f6a318a3b631179902764428bb4b9
โมกข์ มาอาจ <owl@Macintosh.local>
2024-08-11 01:47:45 +0700

    import routerModule in component.ts

--- frontend/src/app/features/page-not-found/page-not-found.component.ts
+++ frontend/src/app/features/page-not-found/page-not-found.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css'
 })

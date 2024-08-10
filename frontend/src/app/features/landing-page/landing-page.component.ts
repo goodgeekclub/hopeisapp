60de2dc644b6588acd5637ab9197e6535ed1a338
โมกข์ มาอาจ <owl@Macintosh.local>
2024-08-10 23:08:19 +0700

    add router link

--- frontend/src/app/features/landing-page/landing-page.component.ts
+++ frontend/src/app/features/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [ RouterModule ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })

d2b02ce5c953fe53bad467bd4efae3799a71611e
viruskizz <kizzaraiva@gmail.com>
2024-08-11 18:19:52 +0700

    fix: subscribe depend

--- frontend/src/app/features/quiz/result/result.component.ts
+++ frontend/src/app/features/quiz/result/result.component.ts
@@ -7,6 +7,7 @@ import { ResultCharacterComponent } from './result-character/result-character.co
 import { ProfileService } from '../../../services/profile.service';
 import { CharacterService } from '../../../services/character.service';
 import { QuizResultService } from '../../../services/quiz-result.service'; // Import QuizResultService
+import { EMPTY, map, Observable, of, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-result',
@@ -36,80 +37,37 @@ export class ResultComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.getResult();
-    this.showResult();
+    this.getResult().pipe(
+      switchMap(result => this.quizResultService.postQuizResult(result))
+    ).subscribe(response => {
+      this.isClikedToShowText = true;
+      this.quizResultId = response._id;
+    })
   }
 
   public showResult(): void {
-    console.log(this.quizResultId);
-    if (this.displayIndex === 0) {
-      this.isClikedToShowText = true;
-      this.displayIndex++;
-    } else if (this.displayIndex === 1 && this.hasResult) {
-      this.isClickedToShowResult = true;
-      this.displayIndex++;
+    if (this.quizResultId) {
       this.router.navigate(['/quiz/result/', this.quizResultId]);
     }
   }
 
-  private getResult(): void {
+  private getResult(): Observable<any> {
     const profile = this.profileService.getProfile();
-    if (profile) {
-      console.log('Profile:', profile);
-      this.character = profile.characterType.trim().toLowerCase();
-      console.log('Profile character type:', this.character);
-      this.hasResult = true;
-
-      this.characterService.getCharacterByType(this.character).subscribe(
-        response => {
-          console.log('Fetched character data:', response);
-          const matchingCharacter = response.find(
-            (char: any) => char.name.trim().toLowerCase() === this.character
-          );
-
-          if (matchingCharacter) {
-            // Prepare the data to be POSTed
-            const postData = {
-              score: profile.characterScore,
-              displayName: profile.user,
-              character: {
-                name: matchingCharacter.data.name,
-                title: matchingCharacter.data.title,
-                description: matchingCharacter.data.description,
-                quote: matchingCharacter.data.quote,
-                detail: matchingCharacter.data.detail,
-                photoUrl: matchingCharacter.data.photoUrl,
-                ability: matchingCharacter.data.ability, // Ensure ability is a string
-                natures: matchingCharacter.data.natures, // Ensure natures is an array
-              },
-            };
-
-            // Flag to prevent multiple POSTs
-            let hasPosted = false;
-
-            this.quizResultService.postQuizResult(postData).subscribe(
-              postResponse => {
-                if (!hasPosted) {
-                  console.log('POST response:', postResponse);
-                  this.quizResultId = postResponse._id;
-                  hasPosted = true;
-                  // this.router.navigate(['', this.quizResultId])
-                }
-              },
-              postError => {
-                console.error('Error posting data:', postError);
-              }
-            );
-          } else {
-            console.log('Character not found.');
-          }
-        },
-        error => {
-          console.error('Error fetching character data:', error);
-        }
-      );
-    } else {
-      console.error('Profile not found');
+    if (!profile) {
+      return of(null);
     }
+    this.character = profile.characterType.trim().toLowerCase();
+    return this.characterService.getCharacterByType(this.character).pipe(
+      map((response: any[]) => {
+        const matchingCharacter = response.find(
+          (char: any) => char.name.trim().toLowerCase() === this.character
+        );
+        return {
+          score: profile.characterScore,
+          displayName: profile.user,
+          character: matchingCharacter.data,
+        }
+      })
+    )
   }
 }
